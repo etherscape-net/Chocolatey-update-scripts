dd92c5897f9d483d0ab1e1f944ccac2a326185fa
svc-git-etherscape <65209274+svc-git-etherscape@users.noreply.github.com>
2023-12-22 14:24:35 -0800

    Update ChocoUpgradeALL.ps1

--- ChocoUpgradeALL.ps1
+++ ChocoUpgradeALL.ps1
@@ -1,6 +1,9 @@
 # Install all updates of packages installed from Chocolately Repos
 choco upgrade all  -y
 
+# Be sure to install Install-Module PSWindowsUpdate -force, first!!
+# https://stackoverflow.com/questions/41117421/ps1-cannot-be-loaded-because-running-scripts-is-disabled-on-this-system
+
 # Query and Install all Windows Updates
 # Get-WindowsUpdate -AcceptAll -Install -IgnoreReboot
 Install-WindowsUpdate -NotCategory "Drivers" -AcceptAll  -IgnoreReboot
