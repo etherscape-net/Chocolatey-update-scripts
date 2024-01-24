417f610395e2123d4f60a5e5255f613867334fdb
svc-git-etherscape <65209274+svc-git-etherscape@users.noreply.github.com>
2024-01-24 00:16:20 -0800

    Delete ChocolateyUpdateall.bat

--- ChocolateyUpdateall.bat
+++
@@ -1,5 +0,0 @@
-REM Call Powershel to update all Chocolately packages from Repos
-Powershell.exe -File C:\ProgramData\chocolatey\ChocoUpgradeALL.ps1
-
-@REM REM Update Office 
-@REM cmd.exe /c  "C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient.exe" /update user
